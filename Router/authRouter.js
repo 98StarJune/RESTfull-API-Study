055d6b441cf4033dc0f447772449f534c4c2fe56
StarJune <partis98@kakao.com>
2022-12-31 19:28:46 +0900

    Validation Create.

--- Router/authRouter.js
+++ Router/authRouter.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const {join} = require('../control/join')
+const {join, joinvalidator} = require('../control/join')
+const {body} = require("express-validator/check");
+
 // /auth/join
 
-router.post('/join', join);
+router.post('/join',[
+    body('id').exists().trim().isLength({min : 5}).withMessage('fail : id length'),
+    body('pw').exists().trim().isLength({min : 6}).withMessage('fail : pw length'),
+    body('email').trim().isEmail().withMessage('fail : not Email')
+], join);
 
 module.exports = router;
--- control/join.js
+++ control/join.js
@@ -1,23 +1,35 @@
 const bcrypt = require('bcryptjs')
 const user = require('../class/user');
+const {validationResult} = require('express-validator/check')
 
 module.exports.join = (req, res, next) => {
+    const err = validationResult(req);
+    if(!err.isEmpty()){
+        console.log(err.array());
+        return res.status(422).json({
+            message : "validation error",
+            error : err.array()[0].msg
+        })
+    }
+
     const id = req.body.id;
     const pw = req.body.pw;
     const name = req.body.name;
     const phone = req.body.phone;
     const email = req.body.email;
+
     bcrypt.hash(pw, 12).then(hashpw=>{
         user.findOne({id: id})
             .then(userinfo => {
                 if (userinfo) {
                     return res.status(200).json({
-                        message: "this ID has already not allowed"
+                        message: "this ID is not allowed"
                     })
                 }
                 const User = new user({
                     id: id,
                     pw: hashpw,
+                    name: name,
                     email: email,
                     pn: phone
                 }).save()
@@ -45,4 +57,4 @@ module.exports.join = (req, res, next) => {
             message : "hash fail"
         })
     })
-}
+}///
